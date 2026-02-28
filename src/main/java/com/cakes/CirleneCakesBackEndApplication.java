e98b9f36c59d9df933e1ab28704289ba21ff0311
taynaflor-gif <tayna.flor@academico.ifpb.edu.br>
2026-02-28 16:17:20 -0300

    setup inicial do projeto

---
+++ src/main/java/com/cakes/CirleneCakesBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.cakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CirleneCakesBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CirleneCakesBackEndApplication.class, args);
+	}
+
+}
