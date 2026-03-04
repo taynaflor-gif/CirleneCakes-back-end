06dafa76785ac80f44c65c0792c0bd138646d21e
taynaflor-gif <tayna.flor@academico.ifpb.edu.br>
2026-03-03 22:58:41 -0300

    Setup inicial do projeto.

---
+++ src/test/java/com/cakes/CirleneCakesBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cakes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CirleneCakesBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
